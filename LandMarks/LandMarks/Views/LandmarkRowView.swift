ac1844f36d0d4f65f28fefbe1b6d1b22b7929b21
Xinghom <Xinghom@users.noreply.github.com>
2020-12-21 00:08:21 -0800

    update gitignore

---
+++ LandMarks/LandMarks/Views/LandmarkRowView.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRowView.swift
+//  LandMarks
+//
+//  Created by Xinghang Xu on 12/18/20.
+//
+
+import SwiftUI
+
+struct LandmarkRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRowView()
+    }
+}
